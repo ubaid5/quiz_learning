5c7da949efab591f84a6791b1fabe0ed5b88442f
ubaid5 <60641452+ubaid5@users.noreply.github.com>
2025-10-08 22:52:22 +0500

    feat: update the di path

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/constants/app_constants.dart';
-import 'core/di/injection_container.dart' as di;
+import 'core/di/dependency_injection.dart' as di;
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'features/quiz/presentation/bloc/quiz_bloc.dart';
